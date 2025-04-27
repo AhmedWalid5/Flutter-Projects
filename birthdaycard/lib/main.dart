9d20f7565449fdaf39c6d07ffa1ba9c814f92cd0
AhmedWalid5 <aw8169112@gmail.com>
2025-04-27 20:21:37 +0300

    is my first project

---
+++ birthdaycard/lib/main.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  const BirthdayCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFFD2BCD5),
+        body: Image(image: AssetImage('images/birthCard.png')),
+      ),
+    );
+  }
+}
